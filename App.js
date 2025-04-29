97e9560db5dadf02206f4df9e1f53233ca2e9045
tousif nawaz <tousifnawaz01@gmail.com>
2025-04-29 09:21:24 +0530

    imported all the necesarry packages to make it run smooth and fine.

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
+import PasscodeScreen from './screens/PasscodeScreen';
+import HomeScreen from './screens/HomeScreen';
+import EmotionScreen from './screens/EmotionScreen';
+import LocationScreen from './screens/LocationScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import SOSScreen from './screens/SOSScreen';
+import GeoFenceScreen from './screens/GeoFenceScreen';
+import CompanionScreen from './screens/CompanionScreen';
+import ContactScreen from './screens/ContactScreen';
+
